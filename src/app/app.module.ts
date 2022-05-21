e7522644af9b3e1420d687327b4d4a7d47b5548c
schale29 <alejofschlegel@gmail.com>
2022-05-21 18:39:43 -0300

    feat: Added about and skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
