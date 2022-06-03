f6cb5b568f866121de5830a8ea56a677dc64a8b4
manuelnikli <92961658+manuelnikli@users.noreply.github.com>
2022-06-02 22:12:42 -0300

    style : updated components

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -36,8 +36,8 @@ export class PokemonService {
   private abilitiesCollection: AngularFirestoreCollection<Ability>;
   abilities: Observable<Ability[]>;
 
-  /* private effectivenessCollection: AngularFirestoreCollection<any>;
-  effectiveness: Observable<any>; */
+  private effectivenessCollection: AngularFirestoreCollection<any>;
+  effectiveness: Observable<any>;
 
 
   constructor(private angularFirestore: AngularFirestore) {
@@ -50,8 +50,8 @@ export class PokemonService {
     this.abilitiesCollection = this.angularFirestore.collection<Ability>('abilities');
     this.abilities = this.abilitiesCollection.valueChanges();
 
-    /* this.effectivenessCollection = this.angularFirestore.collection<any>('effectiveness');
-    this.effectiveness = this.effectivenessCollection.valueChanges(); */
+    this.effectivenessCollection = this.angularFirestore.collection<any>('effectiveness');
+    this.effectiveness = this.effectivenessCollection.valueChanges();
   }
 
 
@@ -80,8 +80,16 @@ export class PokemonService {
         defence: effectiveness[1][str]
       });
       this.defenceKeys = Object.keys(this.currentEffectiveness.defence);
-    });
 
+      // Sort defence keys
+      this.defenceKeys = Object.keys(this.currentEffectiveness.defence).sort(
+        (a:any, b:any) => {
+          if( (a === "Inmune" ? 0 : (a.length > 3 ? a[0] / a[2] : a[0])) > (b === "Inmune" ? 0 : (b.length > 3 ? b[0] / b[2] : b[0]) )) {
+            return -1;
+          } else return 1
+        }
+      );
+    });
   }
   setCurrentMoves(moves: Move[]) {
     this.currentMoves = moves;
@@ -116,10 +124,64 @@ export class PokemonService {
     });
   } */
 
-  /* addEffectiveness() {
-    this.effectivenessCollection.doc('attack').set({});
-  } */
+  // Upload effectiveness json to firestore
+  async getJsonData1() {
+    let res = await fetch('assets/data/effectiveness.json');
+    let data = await res.text();
+    return JSON.parse(data);
+  }
 
+  addEffectiveness() {
+    let res = this.getJsonData1().then(data => {
+    let newEffectiveness: any = {};
+    for (let key in data) {
+      let newKey:any = {}
+      for (let key2 in data[key]) {
+        if(data[key][key2].length > 0) {
+          newKey[key2] = data[key][key2];
+        }
+      }
+      newEffectiveness[key] = newKey;
+    }
+    data = newEffectiveness
+
+    let newEffectiveness2: any = {};
+    for (let key in data) {
+      let newKey:any = {}
+      for (let key2 in data[key]) {
+        switch (key2){
+          case "4X":
+            newKey["4X"] = data[key][key2];
+            break;
+          case "2X":
+            newKey["2X"] = data[key][key2];
+            break;
+          case "1X":
+            newKey["1X"] = data[key][key2];
+            break;
+          case ".5X":
+            newKey["1/2X"] = data[key][key2];
+            break;
+          case ".25X":
+            newKey["1/4X"] = data[key][key2];
+            break;
+          case "0X":
+            newKey["Inmune"] = data[key][key2];
+            break;
+        }
+      }
+      newEffectiveness2[key] = newKey;
+    }
+      data = newEffectiveness2;
+      this.effectivenessCollection.doc('defence').set(data);
+    });
+  }
+
+  sortEffectiveness(effectiveness: any){
+
+  }
+
+/*
   deleteEmptyEfectivenessType(effectiveness:any) {
     let newEffectiveness: any = {};
     for (let key in effectiveness[1]) {
@@ -134,4 +196,37 @@ export class PokemonService {
     console.log(newEffectiveness);
     this.angularFirestore.collection<any>('effectiveness').doc('defence').set(newEffectiveness);
   }
+
+  remapEfectivenessTypeNames(effectiveness:any) {
+    let newEffectiveness: any = {};
+    for (let key in effectiveness[1]) {
+      let newKey:any = {}
+      for (let key2 in effectiveness[1][key]) {
+        switch (key2){
+          case "4X":
+            newKey["Takes 4X from"] = effectiveness[1][key][key2];
+            break;
+          case "2X":
+            newKey["Takes 2X from"] = effectiveness[1][key][key2];
+            break;
+          case "1X":
+            newKey["Takes 1X from"] = effectiveness[1][key][key2];
+            break;
+          case ".5X":
+            newKey["Takes 1/2X from"] = effectiveness[1][key][key2];
+            break;
+          case ".25X":
+            newKey["Takes 1/4X from"] = effectiveness[1][key][key2];
+            break;
+          case "0X":
+            newKey["Inmune to"] = effectiveness[1][key][key2];
+            break;
+        }
+      }
+      newEffectiveness[key] = newKey;
+    }
+    console.log(newEffectiveness);
+    this.angularFirestore.collection<any>('effectiveness').doc('defence').set(newEffectiveness);
+  }
+  */
 } 
